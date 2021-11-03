4f8ffa326927d12b19efb715d7a6cc2c2ce081e0
Pedro Dias <pedrodias504@gmail.com>
2021-11-03 17:08:00 -0300

    Callback para modificação do valor do checkbox

--- src/components/CheckboxInput/CheckboxInput.tsx
+++ src/components/CheckboxInput/CheckboxInput.tsx
@@ -4,6 +4,7 @@ import { CheckboxContainer, CheckboxSelector, CheckboxLabel } from "./CheckboxIn
 interface Props {
     name: string,
     value: string,
+    onChange?: (newValue: boolean) => void
 }
 
 export function CheckboxInput(props: Props) {
@@ -12,6 +13,7 @@ export function CheckboxInput(props: Props) {
         type="checkbox" 
         name={props.name} 
         value={props.value}
+        onChange={(event) => props.onChange && props.onChange(!!event.target.value)}
         />
         <CheckboxLabel htmlFor={props.name}>{props.value}</CheckboxLabel>
     </CheckboxContainer>
