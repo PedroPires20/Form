0d52905217cac8f55de195c6ab138566bb21e2eb
Pedro Dias <pedrodias504@gmail.com>
2021-11-29 10:11:38 -0300

    Lógica para adição de campos no Builder

--- src/pages/Builder/Builder.tsx
+++ src/pages/Builder/Builder.tsx
@@ -1,4 +1,5 @@
-import { useForm } from "react-hook-form"
+import { useState } from "react"
+import { useForm, FormProvider } from "react-hook-form"
 import {InputType} from "../../components/InputType/InputType"
 import { TextInput } from "../../components/TextInput/TextInput"
 import {
@@ -9,35 +10,49 @@ import {
   BuilderTitle,
   BuilderFields,
 } from "./BuilderStyles"
+import { FieldBuilder } from "../../components/FieldBuilder/FieldBuilder"
+
+interface BuilderFormValues {
+  name: string
+  description: string
+  inputs: {
+    name: string
+    type: string
+    label: string
+    placeholder?: string
+  }[]
+}
 
 export function Builder() {
-  const { register, getValues } = useForm()
+  const [fields, setFields] = useState<JSX.Element[]>([])
+  const methods = useForm<BuilderFormValues>()
+
+  function handleFieldAdd(inputType: string) {
+    let currentFields = fields.slice()
+    currentFields.push(<FieldBuilder index={currentFields.length} defaultName="Novo input" defaultType={inputType}/>)
+    setFields(currentFields)
+  }
 
   return (
-    <BuilderContainer>
-      <BuilderForm>
-        <BuilderTitle>Title</BuilderTitle>
-        <BuilderDescription>Description</BuilderDescription>
-
-        <BuilderFields>
-          <TextInput
-            register={register}
-            name="test"
-            label="Campo de teste"
-            required
-            placeholder="Insira alguma coisa"
-          />
-        </BuilderFields>
-        <InputType onChange={() => {}}/>
-        <BuilderSubmit
-          onClick={(e) => {
-            e.preventDefault()
-            console.log(getValues())
-          }}
-        >
-          Clique me!
-        </BuilderSubmit>
-      </BuilderForm>
-    </BuilderContainer>
+    <FormProvider {...methods}>
+      <BuilderContainer>
+        <BuilderForm>
+          <BuilderTitle>Title</BuilderTitle>
+          <BuilderDescription>Description</BuilderDescription>
+          <BuilderFields>
+            {fields}
+          </BuilderFields>
+          <InputType onChange={handleFieldAdd}/>
+          <BuilderSubmit
+            onClick={(e) => {
+              e.preventDefault()
+              console.log(methods.getValues())
+            }}
+          >
+            Salvar
+          </BuilderSubmit>
+        </BuilderForm>
+      </BuilderContainer>
+    </FormProvider>
   )
 }
